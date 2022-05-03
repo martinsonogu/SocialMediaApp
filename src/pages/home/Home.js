dd4f1e0005624b6be1272b3de2a1f054e632e4a5
ONOGU Martins Ojonugwam <onogumartins01@gmail.com>
2022-05-03 16:27:49 +0100

    Created and styled the Navbar component

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Topbar from '../../components/Topbar/Topbar';
+
+
+const Home = () => {
+  return (
+    <div>
+      <Topbar/>
+    </div>
+  )
+}
+
+export default Home;
